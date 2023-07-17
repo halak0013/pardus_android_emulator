cdc77be4fbf4747cf118c35abadb24ab480332b8
Muhammet <zumruduanka0013@gmail.com>
2023-07-17 20:15:54 +0300

    adding test senario

---
+++ test/test2.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+
+#export JAVA_HOME=$HOME/.android-emulator/jdk-18
+export ANDROID_SDK_ROOT=$HOME/.android-emulator/sdk
+export ANDROID_EMULATOR_HOME=$HOME/.android-emulator/emulator
+export ANDROID_AVD_HOME=$HOME/.android-emulator/avd
+export ANDROID_USER_HOME=$HOME/.android-emulator/
+export ANDROID_HOME=$HOME/.android-emulator/sdk
+export PATH=$HOME/.android-emulator/sdk/cmdline-tools/latest/bin:$PATH
+export PATH=$HOME/.android-emulator/sdk/platform-tools:$PATH
+export PATH=$HOME/.android-emulator/sdk/emulator:$PATH
+#export PATH=$HOME/.android-emulator/jdk-18/bin:$PATH
+
+
+cd $HOME/.android-emulator/sdk
+
+#yes | sdkmanager --channel=0 --sdk_root=$HOME/.android-emulator/sdk "system-images;android-32;google_apis_playstore;x86_64"
+#yes | sdkmanager --channel=0 --sdk_root=$HOME/.android-emulator/sdk "platforms;android-32"
+#yes "" | avdmanager -v create avd --name "armut" --package "system-images;android-32;google_apis_playstore;x86_64"
+
+
+#mkdir -p $HOME/.android-emulator/userdata/
+
+exec emulator -netfast -writable-system -data $HOME/.android-emulator/userdata/test5.img  -avd "test5" -qemu -cpu host 
+#emulator -help
