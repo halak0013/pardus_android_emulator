1cb0fc23a7d1f51f689491a40f0438b40320fdc1
Muhammet <zumruduanka0013@gmail.com>
2023-07-31 19:58:13 +0300

    update translation

--- update-translations.sh
+++ update-translations.sh
@@ -12,13 +12,13 @@ fi
 
 echo "updating pot file"
 xgettext -o po/pardus-android-emulator.pot --files-from=po/files
-
-for lang in ${langs[@]}; do
-	if [[ -f po/$lang.po ]]; then
-		echo "updating $lang.po"
-		msgmerge -o po/$lang.po po/$lang.po po/pardus-android-emulator.pot
-	else
-		echo "creating $lang.po"
-		cp po/pardus-android-emulator.pot po/$lang.po
-	fi
-done
+#
+#for lang in ${langs[@]}; do
+#	if [[ -f po/$lang.po ]]; then
+#		echo "updating $lang.po"
+#		msgmerge -o po/$lang.po po/$lang.po po/pardus-android-emulator.pot
+#	else
+#		echo "creating $lang.po"
+#		cp po/pardus-android-emulator.pot po/$lang.po
+#	fi
+#done
